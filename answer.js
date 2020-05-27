9858ee7bfd923df8ea7fcd18a3fed7e1f42b3666
Linawaty <linawatylim@gmail.com>
2020-05-27 23:22:03 +0800

    answer on the intermediate CRUD mongo completed

--- answer.js
+++ answer.js
@@ -57,3 +57,69 @@
     $set: { ship: 'The Milano 2'}
 }) */
 
+// Find the INTERMEDIATE_MONGO.md under Advanced Mongo folder in the instructor notes directory. Follow through each of the explanations. Follow the commands and perform appropriate finds after each update call to see the results
+
+// Find the bounties that are greater than 100000
+/* db.bounties.find({
+    reward: { $gt: 100000 }
+}).pretty() */
+
+// Find the bounties that are less than 1000
+/* db.bounties.find({
+    reward: { $lt: 1000 }
+}).pretty() */
+
+// Find the bounties that are less than or equal to 1000
+/* db.bounties.find({
+    reward: { $lte: 1000 }
+}).pretty() */
+
+// Find the bounty with the hunter Nebula
+/* db.bounties.find({
+    hunters: 'Nebula' 
+}).pretty() */
+
+// Find the bounty with the ship Waverider OR Serenity
+/* db.bounties.find({ 
+    $or: [
+        { ship: 'Waverider' },
+        { ship: 'Serenity' }
+    ]
+}).pretty() */
+
+// Find the bounty who is not captured AND has whose client is Ayesha High Priestess of the Sovereign
+/* db.bounties.find({ 
+    captured:false,
+    client: 'Ayesha High Priestess of the Sovereign'
+}).pretty() */
+
+// Increase all the bounties by 333333
+/* db.bounties.updateMany({},
+    { $inc: { reward: 333333 }}) */
+
+// Multiply all the bounties by 2
+/* db.bounties.updateMany({},
+    { $mul: { reward: 2 }}) */
+
+// Add Bobba Fett as a hunter for Malcolm Reynolds
+/* db.bounties.update({ name: 'Malcolm Reynolds' },
+    { $push: { hunters: 'Bobba Fett' } }) */
+
+// Add Bobba Fett as a hunter for the one that has the ship Waverider
+/* db.bounties.update({ ship: 'Waverider' },
+    { $push: { hunters: 'Bobba Fett' } }) */
+
+// Find and remove Dengar the bounty hunter
+/* db.bounties.update({ hunters: 'Dengar' },
+    { $pull: { hunters: 'Dengar' } }) */
+
+// Upserts will insert a value if it doesn't exist, if it does it will update it
+// Try giving a new field of lastSeen to Han Solo, with the property yesterday set upsert to true
+/* db.bounties.update({ name: 'Han Solo' },
+    { $set: { lastSeen: 'Yesterday' } },
+    { upsert : true }) */
+
+// Try giving all bounties a new field of lastSeen - with a value of last week and set upsert to false
+/* db.bounties.updateMany({},
+    { $set: { lastSeen: 'Last Week' } },
+    { upsert : false }) */
