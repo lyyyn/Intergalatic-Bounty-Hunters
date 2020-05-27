bf2344105e54b08c82ac9bfabff7f73660ab77ce
Linawaty <linawatylim@gmail.com>
2020-05-27 22:50:39 +0800

    answer on the basic mongo CRUD completed

--- answer.js
+++ answer.js
@@ -20,7 +20,40 @@
                 'Maid'
         ],
         captured : false
-}) */
+}) 
+
+   db.bounties.find({ client : 'Lina' }).pretty() */
 
 // Do a query to just return the names of all the bounties
-/*  */
+/* db.bounties.find({},{
+    name : 1,
+    _id: -1
+}).pretty() */
+
+// Remove
+// Starbuck and the Captain have come to an understanding. Remove her record
+/* db.bounties.remove({
+    name : 'Starbuck'
+}) */
+// find and remove the duplicate record - be sure to JUST remove the one copy
+/* db.bounties.deleteOne({
+    name : 'Han Solo'
+}) */
+
+// Update
+// Update Sara Lance's name to be her superhero alias 'White Canary'
+/* db.bounties.update({
+    name: 'Sara Lance'
+},
+{
+    $set: { name: 'White Canary'}
+}) */
+
+// Update Rocket's ship to be The Milano 2
+/* db.bounties.update({
+    ship: 'The Milano'
+},
+{
+    $set: { ship: 'The Milano 2'}
+}) */
+
